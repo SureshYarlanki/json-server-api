1ca1713c8832a615fffbc4421b2bca2546712083
SureshYarlanki <143301284+SureshYarlanki@users.noreply.github.com>
2025-02-23 10:07:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // Ensure db.json exists
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 10000;
 
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
-
